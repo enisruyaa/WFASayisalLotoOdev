a22adc7aed1531088e314e4125dd7ee366f30933
enisruyaa@gmail.com <enisruyaa@gmail.com>
2024-02-23 23:14:41 +0300

    Add project files.

---
+++ WFASayisalLotoOdev/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace WFASayisalLotoOdev
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstSonuc1 = new System.Windows.Forms.ListBox();
+            this.lstSonuc2 = new System.Windows.Forms.ListBox();
+            this.lstSonuc3 = new System.Windows.Forms.ListBox();
+            this.lstSonuc4 = new System.Windows.Forms.ListBox();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lstSonuc1
+            // 
+            this.lstSonuc1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lstSonuc1.ForeColor = System.Drawing.Color.MediumBlue;
+            this.lstSonuc1.FormattingEnabled = true;
+            this.lstSonuc1.ItemHeight = 25;
+            this.lstSonuc1.Location = new System.Drawing.Point(58, 133);
+            this.lstSonuc1.Name = "lstSonuc1";
+            this.lstSonuc1.Size = new System.Drawing.Size(151, 179);
+            this.lstSonuc1.TabIndex = 0;
+            // 
+            // lstSonuc2
+            // 
+            this.lstSonuc2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lstSonuc2.ForeColor = System.Drawing.Color.MediumBlue;
+            this.lstSonuc2.FormattingEnabled = true;
+            this.lstSonuc2.ItemHeight = 25;
+            this.lstSonuc2.Location = new System.Drawing.Point(243, 133);
+            this.lstSonuc2.Name = "lstSonuc2";
+            this.lstSonuc2.Size = new System.Drawing.Size(151, 179);
+            this.lstSonuc2.TabIndex = 0;
+            // 
+            // lstSonuc3
+            // 
+            this.lstSonuc3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lstSonuc3.ForeColor = System.Drawing.Color.MediumBlue;
+            this.lstSonuc3.FormattingEnabled = true;
+            this.lstSonuc3.ItemHeight = 25;
+            this.lstSonuc3.Location = new System.Drawing.Point(423, 133);
+            this.lstSonuc3.Name = "lstSonuc3";
+            this.lstSonuc3.Size = new System.Drawing.Size(151, 179);
+            this.lstSonuc3.TabIndex = 0;
+            // 
+            // lstSonuc4
+            // 
+            this.lstSonuc4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lstSonuc4.ForeColor = System.Drawing.Color.MediumBlue;
+            this.lstSonuc4.FormattingEnabled = true;
+            this.lstSonuc4.ItemHeight = 25;
+            this.lstSonuc4.Location = new System.Drawing.Point(607, 133);
+            this.lstSonuc4.Name = "lstSonuc4";
+            this.lstSonuc4.Size = new System.Drawing.Size(151, 179);
+            this.lstSonuc4.TabIndex = 0;
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnEkle.Location = new System.Drawing.Point(364, 348);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(75, 31);
+            this.btnEkle.TabIndex = 1;
+            this.btnEkle.Text = "Ekle";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 28F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(303, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(170, 44);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Bol Şans";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::WFASayisalLotoOdev.Properties.Resources.loto;
+            this.pictureBox1.Location = new System.Drawing.Point(4, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(56, 36);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(108, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 31);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "A";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(305, 91);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 31);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "B";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(482, 91);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(34, 31);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "C";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(670, 91);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(34, 31);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "D";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Crimson;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.lstSonuc4);
+            this.Controls.Add(this.lstSonuc3);
+            this.Controls.Add(this.lstSonuc2);
+            this.Controls.Add(this.lstSonuc1);
+            this.ForeColor = System.Drawing.Color.MediumBlue;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstSonuc1;
+        private System.Windows.Forms.ListBox lstSonuc2;
+        private System.Windows.Forms.ListBox lstSonuc3;
+        private System.Windows.Forms.ListBox lstSonuc4;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ WFASayisalLotoOdev/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFASayisalLotoOdev
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Random r = new Random();
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            foreach (Control item in Controls)
+            {
+                if (item is ListBox)
+                {
+                    ListBox yakalananEleman = item as ListBox;
+                    yakalananEleman.Items.Clear();
+                    int rastgeleSayi = r.Next(1 , 51);
+                    for (int i = 0; i < 6;)
+                    {
+                        if (yakalananEleman.Items.Contains(rastgeleSayi)) // Bu kısımda dedik ki  listboxın içerisinde aynı değişken var mı eğer varsa yeni bir sayı oluştur.
+                        {
+                            rastgeleSayi = r.Next( 1, 50);
+                        }
+                        else
+                        {
+                            yakalananEleman.Items.Add(rastgeleSayi); // Yoksa ekrana yazdır ve i yi bir arttır.
+                            i++;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
